85ba3b082c7cb30820b6cc6465032eaf90005297
proni <pronii@88.com>
2025-12-06 04:01:03 +0800

    feat: add admin send email api

--- mail-worker/src/api/all-email-api.js
+++ mail-worker/src/api/all-email-api.js
@@ -1,10 +1,16 @@
 import app from '../hono/hono';
 import emailService from '../service/email-service';
 import result from '../model/result';
+import userContext from '../security/user-context';
 
 app.get('/allEmail/list',async (c) => {
 	const data = await emailService.allList(c, c.req.query());
 	return c.json(result.ok(data));
+})
+
+app.post('/allEmail/send', async (c) => {
+	const email = await emailService.send(c, await c.req.json(), userContext.getUserId(c));
+	return c.json(result.ok(email));
 })
 
 app.delete('/allEmail/delete',async (c) => {
